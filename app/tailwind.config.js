14fb8b7c757b59cf9030d435697d875109ad34d3
Sam Coupland <samcoupland@gmail.com>
2024-03-26 13:36:00 +0000

    feat: welcome page - 1.0.0

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,22 +1,30 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: "selector",
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
     ],
-
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                serif: ["nato serif", ...defaultTheme.fontFamily.serif],
+                sans: ["lato", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "primary-light": "#f4ede4",
+                "primary-color" : "#e2802e",
+                "primary-dark": "#d36d09",
+                "primary-highlight": "#fcbb7c",
+                "main-dark": "#283944",
+                "main-light": "#f5f5f5",
             },
         },
     },
-
     plugins: [forms],
 };
