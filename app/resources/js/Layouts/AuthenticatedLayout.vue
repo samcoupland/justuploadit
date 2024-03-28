87a9faee6e3a138684240c92112857c832f12b3f
Sam Coupland <samcoupland@gmail.com>
2024-03-28 06:59:35 +0000

    feat: upload dashboard - 1.0.0

--- app/resources/js/Layouts/AuthenticatedLayout.vue
+++ app/resources/js/Layouts/AuthenticatedLayout.vue
@@ -7,65 +7,65 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div>
-        <div class="min-h-screen bg-primary-highlight">
-            <nav class="bg-primary-highlight">
-                <!-- Primary Navigation Menu -->
-                <div class="max-w-7xl mx-auto px-4">
-                    <div class="flex justify-between h-16">
-                        <div class="flex">
-                            <!-- Logo -->
-                            <div class="shrink-0 flex items-center w-8">
-                                <Link :href="route('dashboard')">
-                                <ApplicationLogo class="block fill-current text-primary-color" />
-                                </Link>
-                            </div>
-                        </div>
+  <div>
+    <div class="min-h-screen bg-primary-highlight">
+      <nav class="bg-primary-highlight">
+        <!-- Primary Navigation Menu -->
+        <div class="max-w-7xl mx-auto px-4">
+          <div class="flex justify-between h-16">
+            <div class="flex">
+              <!-- Logo -->
+              <div class="shrink-0 flex items-center w-8">
+                <Link :href="route('dashboard')">
+                <ApplicationLogo />
+                </Link>
+              </div>
+            </div>
 
-                        <div class="flex items-center ms-6">
-                            <!-- Settings Dropdown -->
-                            <div class="ms-3 relative">
-                                <Dropdown align="right" width="48">
-                                    <template #trigger>
-                                        <span class="inline-flex rounded-md">
-                                            <button type="button"
-                                                class="inline-flex items-center px-3 py-2 border border-transparent text-md leading-4 font-medium rounded-md text-main-dark bg-primary-highlight hover:text-primary-dark focus:outline-none transition ease-in-out duration-150">
-                                                {{ $page.props.auth.user.name }}
+            <div class="flex items-center ms-6">
+              <!-- Settings Dropdown -->
+              <div class="ms-3 relative">
+                <Dropdown align="right" width="48">
+                  <template #trigger>
+                    <span class="inline-flex rounded-md">
+                      <button type="button"
+                        class="inline-flex items-center px-3 py-2 border border-transparent text-md leading-4 font-medium rounded-md text-main-dark bg-primary-highlight hover:text-primary-dark focus:outline-none transition ease-in-out duration-150">
+                        {{ $page.props.auth.user.name }}
 
-                                                <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20" fill="currentColor">
-                                                    <path fill-rule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clip-rule="evenodd" />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </template>
+                        <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                          fill="currentColor">
+                          <path fill-rule="evenodd"
+                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                            clip-rule="evenodd" />
+                        </svg>
+                      </button>
+                    </span>
+                  </template>
 
-                                    <template #content>
-                                        <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>
-                                        <DropdownLink :href="route('logout')" method="post" as="button">
-                                            Log Out
-                                        </DropdownLink>
-                                    </template>
-                                </Dropdown>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-
-            <!-- Page Heading -->
-            <header class="bg-main-light shadow" v-if="$slots.header">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <slot name="header" />
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                <slot />
-            </main>
+                  <template #content>
+                    <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>
+                    <DropdownLink :href="route('logout')" method="post" as="button">
+                      Log Out
+                    </DropdownLink>
+                  </template>
+                </Dropdown>
+              </div>
+            </div>
+          </div>
         </div>
+      </nav>
+
+      <!-- Page Heading -->
+      <header class="bg-main-light shadow" v-if="$slots.header">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <slot name="header" />
+        </div>
+      </header>
+
+      <!-- Page Content -->
+      <main>
+        <slot />
+      </main>
     </div>
+  </div>
 </template>
--- app/resources/js/Pages/Dashboard/Dashboard.vue
+++ app/resources/js/Pages/Dashboard/Dashboard.vue
@@ -7,27 +7,26 @@ import FileLibrary from './Partials/FileLibrary.vue';
 </script>
 
 <template>
+    <Head title="Upload" />
 
-  <Head title="Upload" />
-
-  <AuthenticatedLayout>
-    <!-- Upload bar -->
-    <div class="pt-2">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="p-4 bg-white overflow-hidden shadow-sm sm:rounded-lg border-l-4 border-primary-color">
-          <FileUploadForm />
+    <AuthenticatedLayout>
+      <!-- Upload bar -->
+      <div class="pt-2">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div class="p-4 bg-white overflow-hidden shadow-sm sm:rounded-lg border-l-4 border-primary-color">
+            <FileUploadForm />
+          </div>
         </div>
       </div>
-    </div>
 
-    <!-- Library -->
-    <div class="pt-6">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="p-4 bg-white overflow-hidden shadow-sm sm:rounded-lg border-l-4 border-primary-color">
-          <FileLibrary />
+      <!-- Library -->
+      <div class="pt-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div class="p-4 bg-white overflow-hidden shadow-sm sm:rounded-lg border-l-4 border-primary-color">
+            <FileLibrary />
+          </div>
         </div>
       </div>
-    </div>
 
-  </AuthenticatedLayout>
+    </AuthenticatedLayout>
 </template>
